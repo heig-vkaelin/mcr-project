f28ba97d9a4bb4bbcc58a4f849772a7a6c538bc0
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-05-26 12:04:31 +0200

    feat: parser is working, changed structure of level files

--- MCR-Projet/src/Main.java
+++ MCR-Projet/src/Main.java
@@ -1,7 +1,21 @@
+import entities.Entity;
 import levels.LevelParser;
+import levels.LevelState;
 
+/**
+ * Point d'entrée de l'application
+ *
+ * @author Nicolas Crausaz
+ */
 public class Main {
     public static void main(String[] args) {
-        LevelParser.parseLevelFile("1.txt");
+        // TMP: test du parser
+        LevelState state = LevelParser.parseLevelFile("1.txt");
+
+        System.out.println(state.getDifficulty());
+        System.out.println(state.getSideSize());
+        for (Entity e: state.getEntities()) {
+            System.out.println(e);
+        }
     }
 }
