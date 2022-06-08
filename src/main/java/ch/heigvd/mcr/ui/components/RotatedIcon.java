223481815b7a3fc481ceaccb9ea1860484d29b3b
Kalane <vkaelin@hey.com>
2022-06-08 11:57:03 +0200

    style: clean comments in RotatedIcon class

--- src/main/java/ch/heigvd/mcr/ui/components/RotatedIcon.java
+++ src/main/java/ch/heigvd/mcr/ui/components/RotatedIcon.java
@@ -8,86 +8,80 @@ import java.awt.Graphics;
 import javax.swing.Icon;
 
 /**
- * The RotatedIcon allows you to change the orientation of an Icon by
- * rotating the Icon before it is painted. This class supports the following
- * orientations:
- * Source: https://tips4java.wordpress.com/2009/04/06/rotated-icon/
+ * Wrapper autour de la classe Icon native de Swing afin de pouvoir les pivoter
+ * facilement.
+ * Code initial: https://tips4java.wordpress.com/2009/04/06/rotated-icon/
  *
  * @author Valentin Kaelin
  */
 public class RotatedIcon implements Icon {
     private final Icon icon;
-
-    private final Direction rotate;
+    private final Direction direction;
 
     /**
-     * Create a RotatedIcon
+     * Crée une nouvelle icône pivotée.
      *
-     * @param icon   the Icon to rotate
-     * @param rotate the direction of rotation
+     * @param icon      : l'Icon Swing à pivoter
+     * @param direction : la direction dans laquelle pivoter l'Icon
      */
-    public RotatedIcon(Icon icon, Direction rotate) {
+    public RotatedIcon(Icon icon, Direction direction) {
         this.icon = icon;
-        this.rotate = rotate;
+        this.direction = direction;
     }
 
     /**
-     * Gets the width of this icon.
-     *
-     * @return the width of the icon in pixels.
+     * @return la largeur de l'icône en pixels
      */
     @Override
     public int getIconWidth() {
-        if (rotate == Direction.DOWN || rotate == Direction.UP)
+        if (direction == Direction.DOWN || direction == Direction.UP)
             return icon.getIconWidth();
         else
             return icon.getIconHeight();
     }
 
     /**
-     * Gets the height of this icon.
-     *
-     * @return the height of the icon in pixels.
+     * @return la hauteur de l'icône en pixels
      */
     @Override
     public int getIconHeight() {
-        if (rotate == Direction.UP || rotate == Direction.DOWN)
+        if (direction == Direction.UP || direction == Direction.DOWN)
             return icon.getIconHeight();
         else
             return icon.getIconWidth();
     }
 
     /**
-     * Paint the icons of this compound icon at the specified location
+     * Dessine l'icône à la position souhaitée
      *
-     * @param c The component on which the icon is painted
-     * @param g the graphics context
-     * @param x the X coordinate of the icon's top-left corner
-     * @param y the Y coordinate of the icon's top-left corner
+     * @param component : le Component sur lequel dessiner
+     * @param graphics  : le contexte graphique
+     * @param x         : la coordonnée X du coin haut-gauche de l'icône
+     * @param y         : la coordonnée Y du coin haut-gauche de l'icône
      */
     @Override
-    public void paintIcon(Component c, Graphics g, int x, int y) {
-        Graphics2D g2 = (Graphics2D) g.create();
+    public void paintIcon(Component component, Graphics graphics, int x, int y) {
+        Graphics2D g2 = (Graphics2D) graphics.create();
 
         int cWidth = icon.getIconWidth() / 2;
         int cHeight = icon.getIconHeight() / 2;
         int xAdjustment = (icon.getIconWidth() % 2) == 0 ? 0 : -1;
         int yAdjustment = (icon.getIconHeight() % 2) == 0 ? 0 : -1;
 
-        if (rotate == Direction.LEFT) {
+        if (direction == Direction.LEFT) {
             g2.translate(x + cHeight, y + cWidth);
             g2.rotate(Math.toRadians(90));
-            icon.paintIcon(c, g2, -cWidth, yAdjustment - cHeight);
-        } else if (rotate == Direction.RIGHT) {
+            icon.paintIcon(component, g2, -cWidth, yAdjustment - cHeight);
+        } else if (direction == Direction.RIGHT) {
             g2.translate(x + cHeight, y + cWidth);
             g2.rotate(Math.toRadians(-90));
-            icon.paintIcon(c, g2, xAdjustment - cWidth, -cHeight);
-        } else if (rotate == Direction.UP) {
+            icon.paintIcon(component, g2, xAdjustment - cWidth, -cHeight);
+        } else if (direction == Direction.UP) {
             g2.translate(x + cWidth, y + cHeight);
             g2.rotate(Math.toRadians(180));
-            icon.paintIcon(c, g2, xAdjustment - cWidth, yAdjustment - cHeight);
-        } else if (rotate == Direction.DOWN) {
-            icon.paintIcon(c, g2, x, y);
+            icon.paintIcon(component, g2, xAdjustment - cWidth, yAdjustment - cHeight);
+        } else if (direction == Direction.DOWN) {
+            icon.paintIcon(component, g2, x, y);
         }
 
         g2.dispose();
