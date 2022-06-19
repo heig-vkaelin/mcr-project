c875e150ab0dd020e9d81a80075e55aa41f34947
Maxime Scharwath <maxscharwath@gmail.com>
2022-06-19 22:22:26 +0200

    Howard Shore composed, orchestrated, conducted and produced the trilogy's music. Shore visited the set in 1999, and composed a version of the Shire theme and Frodo's Theme before Jackson began shooting.[48] In August 2000 he visited the set again, and watched the assembly cuts of The Fellowship of the Ring and The Return of the King.[49] In the music, Shore included many (85 to 110) leitmotifs to represent various characters, cultures and places—the largest catalogue of leitmotifs in the history of cinema, surpassing, for comparison, that of the entire Star Wars film series. For example, there are multiple leitmotifs just for the hobbits and the Shire. Although the first film had some of its score recorded in Wellington, virtually all of the trilogy's score was recorded in Watford Town Hall and mixed at Abbey Road Studios.[30] Jackson planned to advise the score for six weeks each year in London, though for The Two Towers he stayed for twelve.[50]
    The score is primarily played by the London Philharmonic Orchestra, ranging from 93 to 120 players throughout the recording. London Voices, the London Oratory School Schola boy choir, and many artists such as Ben Del Maestro, Sheila Chandra, Enya, Renée Fleming, James Galway, Annie Lennox and Emilíana Torrini contributed. Even actors Billy Boyd, Viggo Mortensen, Liv Tyler, Miranda Otto (extended cuts only for the latter two) and Peter Jackson (for a single gong sound in the second film) contributed to the score. Fran Walsh and Philippa Boyens served as librettists, writing lyrics to various music and songs, which David Salo translated into Tolkien's languages. The third film's end song, "Into the West", was a tribute to a young filmmaker Jackson and Walsh befriended named Cameron Duncan, who died of cancer in 2003.[51]
    
    Shore composed a main theme for the Fellowship rather than many different character themes, and its strength and weaknesses in volume are depicted at different points in the series. On top of that, individual themes were composed to represent different cultures. Infamously, the amount of music Shore had to write every day for the third film increased dramatically to around seven minutes.[51] The music for the series has been voted best movie soundtrack of all time for the six years running, passing Schindler's List (1993), Gladiator (2000), Star Wars (1977) and Out of Africa (1985), respectively.[52]

--- src/main/java/ch/heigvd/mcr/Main.java
+++ src/main/java/ch/heigvd/mcr/Main.java
@@ -51,7 +51,6 @@ public class Main {
 
         AssetManager.audios.register("death", new AudioAssetLoader("audio/death.wav"));
         AssetManager.audios.register("horn", new AudioAssetLoader("audio/horn.wav"));
-        AssetManager.audios.register("music", new AudioAssetLoader("audio/music.wav"));
         AssetManager.audios.register("sw", new AudioAssetLoader("audio/sw.wav"));
         AssetManager.audios.register("bonk", new AudioAssetLoader("audio/bonk.wav"));
         AssetManager.audios.register("menu", new AudioAssetLoader("audio/menu.wav"));
